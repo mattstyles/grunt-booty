d7f49f56625479ada8a4133dc267ecd0607791b2
Matt Styles <matt@veryfizzyjelly.com>
2013-04-20 14:08:21 +0100

    Added first test of copying over files into a blank file structure

--- test/bower_bootstrap_test.js
+++ test/bower_bootstrap_test.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var grunt = require('grunt');
+var grunt = require( 'grunt' )
+  , fs    = require( 'fs' );
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -23,26 +24,50 @@ var grunt = require('grunt');
 */
 
 exports.bower_bootstrap = {
-  setUp: function(done) {
-    // setup here if necessary
-    done();
-  },
-  default_options: function(test) {
-    test.expect(1);
 
-    var actual = grunt.file.read('tmp/default_options');
-    var expected = grunt.file.read('test/expected/default_options');
-    test.equal(actual, expected, 'should describe what the default behavior is.');
+    setUp: function( done ) {
+        // setup here if necessary
+        done();
+    },
 
-    test.done();
-  },
-  custom_options: function(test) {
-    test.expect(1);
+    tearDown: function( done ) {
+        // tear down here if necessary
+        done();
+    },
+//  default_options: function(test) {
+//    test.expect(1);
+//
+//    var actual = grunt.file.read('tmp/default_options');
+//    var expected = grunt.file.read('test/expected/default_options');
+//    test.equal(actual, expected, 'should describe what the default behavior is.');
+//
+//    test.done();
+//  },
+//  custom_options: function(test) {
+//    test.expect(1);
+//
+//    var actual = grunt.file.read('tmp/custom_options');
+//    var expected = grunt.file.read('test/expected/custom_options');
+//    test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
+//
+//    test.done();
+//  }
 
-    var actual = grunt.file.read('tmp/custom_options');
-    var expected = grunt.file.read('test/expected/custom_options');
-    test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
+    /*
+     * test1
+     *
+     * /styles/ has /less/ and /css/ folders but nothing more.
+     * Expected - task should populate /styles/ with bootstrap less files
+     */
+    test1: function( test ) {
+        test.expect(1);
 
-    test.done();
-  },
+        var actual = fs.readdirSync( 'test/test1/styles').sort();
+        var expected = fs.readdirSync( 'test/expected/test1/styles').sort();
+
+        test.deepEqual( actual, expected, 'should copy over bootstrap less files and font-awesome-more files using the correct file structure' );
+
+        test.done();
+    }
+
 };
